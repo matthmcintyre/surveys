773a1f95118d3f7b1d3fbd10a8098c487dd506c7
Matthew McIntyre <mmcintyre@23andme.com>
2022-02-01 13:20:19 -0800

    Survey Docs 15DEC2021 (commit 5)

--- index.py
+++ index.py
@@ -13,17 +13,16 @@ filepaths = collections.OrderedDict({dirname: [os.path.join(root, name)
              for root, dirs, files in os.walk(dirname)
              for name in files if name.endswith(".html")] for dirname in dirnames})
 
-
 for directory in filepaths.keys():
     out+= "  <LI>" + directory + "</LI>\n"
     out+= "  <UL>"
-    for file in filepaths[directory]:
+    dirfiles = filepaths[directory]
+    dirfiles.sort()
+    for file in dirfiles:
         name = file.split(os.path.sep)[-1].replace('.html', '')
-        out+= "    <LI><a href=" + file + ">" + "</a></LI>"
+        out+= "    <LI><a href=" + file.replace(' ', '%20') + ">" + name + "</a></LI>\n"
     out+= "  </UL>"
 out+= "</UL>"
 
 with codecs.open('index.html', 'w', encoding="utf-8") as f:
     f.write(out)
-
-[(root, dirs, files) for root, dirs, files in os.walk('.')]
